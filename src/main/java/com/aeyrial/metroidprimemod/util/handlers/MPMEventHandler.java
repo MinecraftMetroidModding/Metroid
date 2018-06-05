e01da6afb0c8b8000e863ccd639b012478a7daf9
Aiolos <cwhoot@gmail.com>
2018-06-05 18:43:08 +1000

    Minor edits

--- src/main/java/com/aeyrial/metroidprimemod/util/handlers/MPMEventHandler.java
+++ src/main/java/com/aeyrial/metroidprimemod/util/handlers/MPMEventHandler.java
@@ -2,12 +2,17 @@ package com.aeyrial.metroidprimemod.util.handlers;
 import com.aeyrial.metroidprimemod.util.handlers.SoundsHandler;
 
 import net.minecraft.block.Block;
+import net.minecraft.client.audio.MovingSound;
+import net.minecraft.client.audio.MovingSoundMinecart;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.util.SoundCategory;
 import net.minecraft.util.SoundEvent;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.biome.Biome;
+import net.minecraftforge.client.event.sound.PlaySoundEvent;
+import net.minecraftforge.client.event.sound.PlaySoundSourceEvent;
+import net.minecraftforge.event.entity.PlaySoundAtEntityEvent;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -18,7 +23,7 @@ import scala.Console;
 
 
 
-public class MPMEventHandler
+public class MPMEventHandler 
 {
 	Biome currentBiome = null;
 	Biome previousBiome = null;
@@ -26,26 +31,25 @@ public class MPMEventHandler
 	@SubscribeEvent
 	public void onMove(TickEvent.PlayerTickEvent event) {
 		
-		int reductionFactor = 30;
-		int checkIterator = 0;
+
 		
 
 		currentBiome = event.player.getEntityWorld().getBiome(event.player.getPosition());
-		if(currentBiome == Biome.getBiome(0)&&(currentBiome!=previousBiome)&&(checkIterator > reductionFactor))
+		if(currentBiome == Biome.getBiome(0)&&(currentBiome!=previousBiome))
 		{
 			
 			// stop current music
-			event.player.world.playSound(event.player, event.player.getPosition(), SoundsHandler.AMBIENCE_TALLON_OVERWORLD_MAIN_THEME, SoundCategory.MUSIC, 0.5F, 1.0F);
 			
+			
+			event.player.world.playSound(event.player, event.player.getPosition(), SoundsHandler.AMBIENCE_TALLON_OVERWORLD_MAIN_THEME, SoundCategory.MUSIC, 0.5F, 1.0F);
+//			MovingSound.this.getSound().
 			System.out.println("Is the music playing?");
-
 		}
 		if((currentBiome!=previousBiome))
 		{
 			previousBiome = currentBiome;
 			currentBiome = null;
 		}
-		checkIterator++;
 	}
 	
 	
